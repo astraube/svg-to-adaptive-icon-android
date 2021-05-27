d5feb9062bee72037413ec74f6ef56601ef28e1f
Andrè Straube <a.straube.m@gmail.com>
2021-05-27 03:56:39 -0300

    Create svg_to_png.sh

---
+++ svg_to_png.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+svgexport ic_launcher_foreground.svg res/drawable-hdpi/ic_launcher_foreground.png 162:162
+svgexport ic_launcher_foreground.svg res/drawable-mdpi/ic_launcher_foreground.png 108:108
+svgexport ic_launcher_foreground.svg res/drawable-xhdpi/ic_launcher_foreground.png 216:216
+svgexport ic_launcher_foreground.svg res/drawable-xxhdpi/ic_launcher_foreground.png 324:324
+svgexport ic_launcher_foreground.svg res/drawable-xxxhdpi/ic_launcher_foreground.png 432:432
+
+svgexport ic_launcher_background.svg res/drawable-hdpi/ic_launcher_background.png 162:162
+svgexport ic_launcher_background.svg res/drawable-mdpi/ic_launcher_background.png 108:108
+svgexport ic_launcher_background.svg res/drawable-xhdpi/ic_launcher_background.png 216:216
+svgexport ic_launcher_background.svg res/drawable-xxhdpi/ic_launcher_background.png 324:324
+svgexport ic_launcher_background.svg res/drawable-xxxhdpi/ic_launcher_background.png 432:432
+
+svgexport logo.svg res/minmap-hdpi/launcher_icon.png 72:72
+svgexport logo.svg res/minmap-mdpi/launcher_icon.png 48:48
+svgexport logo.svg res/minmap-xhdpi/launcher_icon.png 96:96
+svgexport logo.svg res/minmap-xxhdpi/launcher_icon.png 144:144
+svgexport logo.svg res/minmap-xxxhdpi/launcher_icon.png 192:192
