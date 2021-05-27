15a796e6ca9c67bf202a4abd536f781c4d9d7a0a
Andrè Straube <a.straube.m@gmail.com>
2021-05-27 03:59:48 -0300

    Create svg_to_png.bat

---
+++ svg_to_png.bat
@@ -0,0 +1,24 @@
+@rem ##########################################################################
+@rem
+@rem  Android Adaptive icon script
+@rem
+@rem ##########################################################################
+
+
+call svgexport ic_launcher_foreground.svg res/drawable-hdpi/ic_launcher_foreground.png 162:162
+call svgexport ic_launcher_foreground.svg res/drawable-mdpi/ic_launcher_foreground.png 108:108
+call svgexport ic_launcher_foreground.svg res/drawable-xhdpi/ic_launcher_foreground.png 216:216
+call svgexport ic_launcher_foreground.svg res/drawable-xxhdpi/ic_launcher_foreground.png 324:324
+call svgexport ic_launcher_foreground.svg res/drawable-xxxhdpi/ic_launcher_foreground.png 432:432
+
+call svgexport ic_launcher_background.svg res/drawable-hdpi/ic_launcher_background.png 162:162
+call svgexport ic_launcher_background.svg res/drawable-mdpi/ic_launcher_background.png 108:108
+call svgexport ic_launcher_background.svg res/drawable-xhdpi/ic_launcher_background.png 216:216
+call svgexport ic_launcher_background.svg res/drawable-xxhdpi/ic_launcher_background.png 324:324
+call svgexport ic_launcher_background.svg res/drawable-xxxhdpi/ic_launcher_background.png 432:432
+
+call svgexport logo.svg res/minmap-hdpi/launcher_icon.png 72:72
+call svgexport logo.svg res/minmap-mdpi/launcher_icon.png 48:48
+call svgexport logo.svg res/minmap-xhdpi/launcher_icon.png 96:96
+call svgexport logo.svg res/minmap-xxhdpi/launcher_icon.png 144:144
+call svgexport logo.svg res/minmap-xxxhdpi/launcher_icon.png 192:192
